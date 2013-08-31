59da2a9d856ae7799f8b7cc8877a1c98133647e9
eryueniaobp <baipeng8608@gmail.com>
2013-08-31 07:53:30 -0700

    一种简洁的写法，O(n^3) 主要优势是 逻辑简洁

--- day6/scramble string.cpp
+++ day6/scramble string.cpp
@@ -1,48 +1,33 @@
+/**
+ * time complexit: O(n^3) 
+ * 采用前一个版本的递归写法 +　状态保存，也是这个复杂度，只是没有这个代码好看
+ */
 class Solution {
+    bool tag[100][100][100];
 public:
     bool isScramble(string s1, string s2) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        return isValid(0,s1.size()-1,0,s2.size()-1,s1,s2);
-    }
-    bool isValid(int l1,int r1,int l2,int r2,string &s1,string &s2)
-    {
-        if(r1 - l1 != r2 - l2)
+        int len = s1.size() ; 
+        if(len  !=  s2.size() ) {
             return false;
-        int key = 1;
-        int mark1[26];
-        int mark2[26];
-        memset(mark1,0,sizeof(mark1));
-        memset(mark2,0,sizeof(mark2));
-        for(int i = 0; i <= r1 - l1;i++)
-        {
-            mark1[s1[l1 + i] - 'a']++;
-            mark2[s2[l2 + i] - 'a']++;
-            if(s1[l1 + i] != s2[l2 + i])
-                key = 0;
         }
-        if(key == 1)
-            return true;
-        if(l1 == r1)
-            return false;
-        for(int i = 0;i < 26;i++)
-        {
-            if(mark1[i] != mark2[i])
-                return false;
-        }
-        for(int i = 0; i <= r1 - l1 -1;i++)
-        {
-            bool tag1,tag2; 
-            tag1 = isValid(l1,l1 + i,r2 - i,r2,s1,s2);
-            tag2 = isValid(l1+i+1,r1,l2,r2-i-1,s1,s2);
-            if(tag1 && tag2)
-                return true;
-            tag1 = isValid(l1,l1 + i,l2,l2 + i,s1,s2);
-            tag2 = isValid(l1+i+1,r1,l2+i+1,r2,s1,s2);
-            if(tag1 && tag2)
-                return true;            
-        }
-        return false;
+        if(len == 0 ) return true;
+        for(int i = 0 ; i< len ;i ++ ) 
+            for(int j = 0 ;j < len ; j++ ) {
+                tag[0][i][j] = ( s1[i] == s2[j] ) ;
+            }
+        for(int k =1 ;  k < len ; k ++ ) 
+            for(int i = len -1 - k  ; i >= 0 ; i -- )
+                for(int j = len -1 - k ; j >= 0 ; j-- ){
+                    bool b = false;
+                    for(int m = 0 ; m < k && !b ; m ++ ) {
+                        b = ( tag[m][i][j] && tag[k-m-1][i+m+1][j+m+1]) || ( tag[m][i][j+k -m] && tag[k-m-1][i+ m +1 ][j] ) ;
+                    }
+                    tag[k][i][j] = b ; 
+                 }
+        return tag[len-1][0][0];
     }
 };
+
 
