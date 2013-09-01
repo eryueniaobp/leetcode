fd477f3e2061c6755660edb9246ce6db073ad77b
eryueniaobp <baipeng8608@gmail.com>
2013-08-31 20:19:59 -0700

    O(K + logK * n)

--- day 13/Merge k Sorted Lists.cpp
+++ day 13/Merge k Sorted Lists.cpp
@@ -6,38 +6,49 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/**
+ * O(k+ nlogk) , n是全部元素的个数,每次调整堆需要  log k时间，建堆 k 
+ */
+struct Node{
+    int val;
+    int pos;
+    bool operator < (const Node& a ) const {
+        return val > a.val;
+    }
+    Node(int v,int p):val(v),pos(p){}
+};
 class Solution {
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         ListNode * head = NULL , *cur=NULL,*pre = NULL; 
-        while(true){
-
-            int min = INT_MAX;
-            int mp = -1;
-            int i = 0 ; 
-            for( ;i < lists.size(); i ++  ){
-                if(lists[i] == NULL ) continue;
-                if(lists[i]->val < min ) {
-                    min = lists[i]->val;
-                    mp = i ;
-                }
-            }
-            if(mp>=0){
-                lists[mp] = lists[mp]->next;
-                if(head == NULL){
-                    head = new ListNode(min);
-                    pre = head;
-                }else{
-                    cur = new ListNode(min);
-                    pre->next = cur;
-                    pre = cur;
-                }
-            }else{
-                return head;
+        vector<Node> vheap;
+        for(int i = 0 ;i<lists.size() ; i ++ ) {
+            if(lists[i] != NULL ) {
+                vheap.push_back(Node(lists[i]->val,i));
             }
         }
+        make_heap(vheap.begin(),vheap.end());
+        while(vheap.size() > 0 ){
+            Node node = vheap.front();
+            lists[node.pos] = lists[node.pos]->next;
+            pop_heap(vheap.begin(),vheap.end());
+            vheap.pop_back();
+            if(lists[node.pos] != NULL ) {
+                vheap.push_back(Node(lists[node.pos]->val,node.pos));
+                push_heap(vheap.begin(),vheap.end());
+            }
+            if(head == NULL){
+                head = new ListNode(node.val);
+                pre = head;
+            }else{
+                cur = new ListNode(node.val);
+                pre->next = cur;
+                pre = cur;
+            }
+        }
+        return head;
     }
 };
 
