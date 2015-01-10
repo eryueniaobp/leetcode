92559dda0d2f46ee15a509fe9997822117763216
bai peng <c_pbai@groupon.com>
2015-01-10 14:10:39 +0800

    random pointer

--- revised/Copy List with Random Pointer.cpp
+++ revised/Copy List with Random Pointer.cpp
@@ -45,4 +45,52 @@ public:
         return r2c[head]; 
     }
 };
+/* A more clear solution */
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    /**
+     * @param head: The head of linked list with a random pointer.
+     * @return: A new head of a deep copy of the list.
+     */
+    public RandomListNode copyRandomList(RandomListNode head) {
+        Map<RandomListNode,RandomListNode> r2c = new HashMap<RandomListNode,RandomListNode>(); 
+        if(head == null ) return null ; 
+        
+        RandomListNode node = head ; 
+        RandomListNode prev = null; 
+        while(node!=null) {
+            if(!r2c.containsKey(node)) {
+                RandomListNode cn = new RandomListNode(node.label) ; 
+                r2c.put(node,cn) ; 
+            }else{
+                
+            }
+            if(node.random!=null && !r2c.containsKey(node.random)) {
+                RandomListNode cr = new RandomListNode(node.random.label) ;
+                r2c.put(node.random,cr) ; 
+            }
+            // always use the Map r2c to get the new node             
+            if(prev != null ) {
+                prev.next = r2c.get(node) ;
+                prev = prev.next ; 
+                prev.random = node.random==null?null:r2c.get(node.random) ; 
+            }else{
+                prev = r2c.get(node) ; 
+                prev.random = node.random==null?null:r2c.get(node.random) ; 
+            }
+            node = node.next ; 
+        }
+        
+        return r2c.get(head) ; 
+        
+    }
+}
+
 
