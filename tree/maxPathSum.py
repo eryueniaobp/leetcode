f67c5ac5c30f94349465e6e12e98fb5af5691953
peng <baipeng8608@gmail.com>
2019-05-24 16:30:05 +0800

    Update maxPathSum.py

--- tree/maxPathSum.py
+++ tree/maxPathSum.py
@@ -5,55 +5,18 @@
 #         self.left = None
 #         self.right = None
 
-import sys
-
 class Solution:
-    
-    result = -sys.maxsize-1
-    
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        self.maxValue(root)
-        return self.result
-    
-    """
-    最大路径和：根据当前节点的角色，路径和可分为两种情况：
-    一：以当前节点为根节点
-    1.只有当前节点
-    2.当前节点+左子树
-    3.当前节点+右子书
-    4.当前节点+左右子树    
-    这四种情况的最大值即为以当前节点为根的最大路径和
-    此最大值要和已经保存的最大值比较，得到整个树的最大路径值
-    
-    二：当前节点作为父节点的一个子节点
-    和父节点连接的话则需取【单端的最大值】
-    1.只有当前节点
-    2.当前节点+左子树
-    3.当前节点+右子书
-    这三种情况的最大值    
-    """
-    def maxValue(self,root):
-        if root == None:            
-            return 0
+    def maxPathSum(self, root: TreeNode) -> int:
+        if root == None: return 0 
+        self.ret = -2**31  
+        def maxPath(node):
+            if node == None: return 0 
+            left = maxPath(node.left)
+            right = maxPath(node.right)
+            
+            if max(left, 0) + node.val + max(right, 0) > self.ret :
+                self.ret = max(left, 0)  + node.val + max(right, 0) 
+            return node.val +  max(max(left, right), 0)
+        maxPath(root)
         
-        leftValue = self.maxValue(root.left)
-        rightValue = self.maxValue(root.right)
-        
-        value1 = root.val
-        value2 = root.val + leftValue
-        value3 = root.val + rightValue
-        value4 = root.val + rightValue + leftValue
-        
-        #以此节点为根节点的最大值
-        maxValue = max([value1,value2,value3,value4])
-        
-        #当前遍历树的最大值
-        self.result = max(maxValue, self.result)
-        
-        #要和父节点关联，则需要取去除情况4的最大值
-        return max([value1,value2,value3])
-        
+        return self.ret 
