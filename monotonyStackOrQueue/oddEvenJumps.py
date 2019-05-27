ef3556ddb9a111f08c0139b136bd38edd52037df
peng <baipeng8608@gmail.com>
2019-05-27 19:07:30 +0800

    Create oddEvenJumps.py

---
+++ monotonyStackOrQueue/oddEvenJumps.py
@@ -0,0 +1,33 @@
+# https://leetcode-cn.com/problems/odd-even-jump/   
+"""
+存在两种 比较因素 以及 极值要求时，使用单调栈是最适合的
+"""
+class Solution(object):
+    def oddEvenJumps(self, A):
+        N = len(A)
+
+        def make(B):
+            ans = [None] * N
+            stack = []  # invariant: stack is decreasing
+            for i in B:
+                while stack and i > stack[-1]:
+                    ans[stack.pop()] = i
+                stack.append(i)
+            return ans
+
+        B = sorted(range(N), key = lambda i: A[i])
+        oddnext = make(B)
+        B.sort(key = lambda i: -A[i])
+        evennext = make(B)
+
+        odd = [False] * N
+        even = [False] * N
+        odd[N-1] = even[N-1] = True
+
+        for i in range(N-2, -1, -1):
+            if oddnext[i] is not None:
+                odd[i] = even[oddnext[i]]
+            if evennext[i] is not None:
+                even[i] = odd[evennext[i]]
+
+        return sum(odd)
