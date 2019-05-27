7376fe56f7bbbc327a957229d80bf7ff6a8714da
peng <baipeng8608@gmail.com>
2019-05-28 07:26:20 +0800

    Create ShortestSubarraywithSumatLeastK.java

---
+++ monotonyStackOrQueue/ShortestSubarraywithSumatLeastK.java
@@ -0,0 +1,19 @@
+/**
+单调队列
+**/
+class Solution {
+    public int shortestSubarray(int[] A, int K) {
+        int N = A.length, res = N + 1;
+        int[] B = new int[N + 1];
+        for (int i = 0; i < N; i++) B[i + 1] = B[i] + A[i];
+        Deque<Integer> d = new ArrayDeque<>();
+        for (int i = 0; i < N + 1; i++)  {
+            
+            while (d.size() > 0 && B[i] - B[d.getFirst()] >=  K)
+                res = Math.min(res, i - d.pollFirst());
+            while (d.size() > 0 && B[i] <= B[d.getLast()]) d.pollLast(); // Deque d keep indexes of increasing B[i]
+            d.addLast(i);
+        }
+        return res <= N ? res : -1;
+    }
+}
